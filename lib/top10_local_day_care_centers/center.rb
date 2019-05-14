19e349fa5b329b096aca3975093f65ba6ce5c76a
chanwkkk <46389283+chanwkkk@users.noreply.github.com>
2019-05-15 07:06:03 +0800

    delete unnecessary comments

--- lib/top10_local_day_care_centers/center.rb
+++ lib/top10_local_day_care_centers/center.rb
@@ -6,22 +6,14 @@ module Top10LocalDayCareCenters
     @@all={}
 
     def initialize
-       #@zip=zip
-       #find_or_create_by_zip with return an array and then anytime we need to push the object to where it belongs
-       #right after it is born
-      #self.class.find_or_create_by_zip <<self
+      
     end
 
     def self.all
       @@all
     end
 
-    #def self.find_or_create_by_zip(zip)
-       #find within @@all to see if that zip is search before or not, if so, return that array
-       #if not, set up a key for that.
-       #self.all[zip]||=[]
-    #end
-
+    
     def self.get_centers_from_zip(zip)
         self.all[zip]=[]
         Scraper.scrape_from_zip(zip)#pushing center objects to self.all[zip] array
